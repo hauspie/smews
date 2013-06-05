1978561ec34dd62b97445eb0dd47cf5d7831c647
Michaël Hauspie <michael.hauspie@lifl.fr>
2013-06-05 10:44:07 +0200

    Fix root path definition

--- tools/build_gui/model/pathNamesConfig.py
+++ tools/build_gui/model/pathNamesConfig.py
@@ -6,7 +6,8 @@ Created on May 28, 2013
 import os
 class PathAndNames:
     def __init__(self, root):
-        self.rootPath=os.path.abspath(os.path.join("..",".."))
+        self.rootPath=os.path.abspath(os.path.join(os.path.dirname(__file__),"..","..",".."))
+        print(self.rootPath)
         self.apps = "apps"
         self.target="targets"
         self.script="run.sh"
