631e8d684272fb57e0cbc6e6dd0582865e1ed2b3
Michaël Hauspie <michael.hauspie@lifl.fr>
2013-06-05 11:25:39 +0200

    Fix linux script to return on successful launch

--- targets/linux/run.sh
+++ targets/linux/run.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
 script_path=`cd \`dirname $0\` && pwd`
-smews_path="$script_path/../.."
-gksudo bin/linux/smews.elf
+smews_path=$script_path/../../
+gksudo $smews_path/bin/linux/smews.elf &
+cpt=1
+while [ $cpt -ne 10 ]
+do
+    pgrep smews.elf && exit 0
+    cpt=`expr $cpt + 1`
+    sleep 1
+done
+exit 1
